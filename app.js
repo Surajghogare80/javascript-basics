77e136243f6eee73d6a194d1cae39522879bed66
Surajghogare80 <surajghogare1001@gmail.com>
2025-02-06 17:07:46 +0530

    js basics switch

---
+++ app.js
@@ -0,0 +1,46 @@
+// let size = "L";
+
+// if (size === "XL"){
+//     console.log("price is 250/-")
+// }
+// else if(size === "L"){
+//     console.log("Price is 200/-")
+// }else if(size === "M"){
+//     console.log("Price is 150/-")
+// }else if(size === "S"){
+//     console.log("Price is 100/-")
+// }
+
+
+let day = 5;
+
+switch (day) {
+    case 1:
+        console.log("sunday")
+        break;
+
+    case 2:
+        console.log("monday")
+        break;
+
+    case 3:
+        console.log("tuesday")
+        break;
+   
+    case 4:
+        console.log("wednesday")
+        break;
+
+    case 5:
+        console.log("thursday")
+        break;
+
+    case 6:
+        console.log("friday")
+        break;
+
+    case 7:
+        console.log("saturday")
+        break;
+  
+}
